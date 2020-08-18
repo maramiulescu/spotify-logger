4c21ad9e5ed97ab650162f40f93b66633c21485f
mara <m.c.miulescu@student.tue.nl>
2020-08-18 17:25:03 +0200

    Fix issue #1

--- api/routes/loggerAPI.js
+++ api/routes/loggerAPI.js
@@ -12,6 +12,14 @@ function millisToMinutesAndSeconds(millis) {
     var seconds = ((millis % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
+/**
+ * Removes commas from a string
+ * @param  {name} Input string with commas
+ * @return {string} Input string with no commas
+ */
+function removeCommas(name) {
+    return name.replace(/,/g, '');
+}
 
 router.get("/", function(req, res, next) {
     res.send("API works!");
@@ -23,9 +31,11 @@ router.post("/", function(req, res, next) {
     var duration = millisToMinutesAndSeconds(req.body.duration_ms);
     var date = new Date(req.body.timestamp).toLocaleDateString("en-GB");
     var time = new Date(req.body.timestamp).toLocaleTimeString("en-GB");
+    var name = removeCommas(req.body.name);
+    var artist = removeCommas(req.body.artist);
     fs.appendFile(
         'log.csv',
-        date + ',' + time + ',' + req.body.name + ',' + req.body.artist + ',' + timePlayed + ',' + duration +'\n',
+        date + ',' + time + ',' + name + ',' + artist + ',' + timePlayed + ',' + duration +'\n',
         function(err) {
         if (err) throw err;
     })
